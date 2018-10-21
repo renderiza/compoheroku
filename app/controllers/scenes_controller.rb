b34873a4421eca51feeea2a1f8b62dd1343d8aa7
Renderiza <renderiza.studio@gmail.com>
2018-10-21 15:44:10 -0700

    Bootstrap and css

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -1,5 +1,6 @@
 class ScenesController < ApplicationController
 	before_action :find_scene, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@scenes = Scene.all
