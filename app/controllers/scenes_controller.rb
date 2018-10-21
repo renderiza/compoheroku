256ce3331d677059bdb17fa572835718e71338eb
Renderiza <renderiza.studio@gmail.com>
2018-10-21 13:14:41 -0700

    second

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -1,4 +1,54 @@
 class ScenesController < ApplicationController
+	before_action :find_scene, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@scenes = Scene.all
 	end
+
+	def show
+	end
+
+	def new
+		@scene = Scene.new
+	end
+
+	def create
+		@scene = Scene.new(scene_params)
+
+		if @scene.save
+			redirect_to @scene, notice: "Successfully created new Scene"
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @scene.update(scene_params)
+			redirect_to @scene, notice: "Scene was Successfully updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@scene.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def scene_params
+		params.require(:scene).permit(
+			:name,
+			:description
+		)
+	end
+
+	def find_scene
+		@scene = Scene.find(params[:id])
+	end
+
 end
