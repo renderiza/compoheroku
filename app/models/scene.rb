47259caa9847b549a9ec44539134ea39b45c5cf5
Renderiza <renderiza.studio@gmail.com>
2018-10-23 09:54:54 -0700

    Fixed local image

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -1,9 +1,14 @@
 class Scene < ApplicationRecord
 	belongs_to :user
-	has_attached_file :image, styles: {
-		medium: "300x300>", 
-		thumb: "144x144>" 
-	}, default_url: "/images/:style/missing.png"
+	has_attached_file :image, 
+		path: ":rails_root/public/system/:attachment/:id/:style/:filename",
+		url: "/system/:attachment/:id/:style/:filename", 
+		styles: {
+			medium: "300x300>", 
+			thumb: "144x144>" 
+		}, 
+		default_url: "/images/:style/missing.png"
+
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 	validates_presence_of :name
